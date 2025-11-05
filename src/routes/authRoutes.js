060285b89dfc418158f7368c6be67c5243637ef4
Mahdi <ialwaysloveyouern139@gmail.com>
2025-11-05 23:37:43 +0330

    اضافه کردن بخش خانهها به بکاند

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -11,7 +11,7 @@ const generateAccessToken = (userId) =>{
    return jwt.sign({userId}, process.env.JWT_SECRET, {expiresIn: "15m"});
 };
 const generateRefreshToken = (userId) => {
-  return jwt.sign({ userId }, process.env.JWT_REFRESH_SECRET, { expiresIn: "7d" });
+  return jwt.sign({ userId }, process.env.JWT_REFRESH_SECRET, { expiresIn: "5m" });
 };
 
 // 🔹 اینجا تعریف کن
