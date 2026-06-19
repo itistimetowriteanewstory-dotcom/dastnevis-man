0360d66958798dd41487f2287dcc5c29d6c38ec5
Mahdi <ialwaysloveyouern139@gmail.com>
2026-06-20 02:29:13 +0330

    lllll

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ import eatRoutes from "./routes/eatRoutes.js";
 import NotificationStatus from "./models/NotificationStatus.js";
 
 const app = express();
-app.set("trust proxy", 2);
+app.set("trust proxy", 1);
 const PORT = process.env.PORT || 3000;
 
 
