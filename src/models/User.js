50aeee170ef35dbb2bafba752edf97162ab668c4
Mahdi <ialwaysloveyouern139@gmail.com>
2026-06-28 20:55:48 +0200

    mahdimosavi

--- src/models/User.js
+++ src/models/User.js
@@ -22,7 +22,7 @@ const userSchema = new mongoose.Schema({
         type: String,
         default: "",
     },
-     profileImage: {
+     profileImagePublicId: {
        type: String,
        default: ""
        },
